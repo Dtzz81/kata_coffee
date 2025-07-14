e0111929720b68d1d7a27ed584b6545d04d4629c
denise.turkova <denise.turkova@madetech.com>
2025-07-14 12:14:29 +0100

    added basic one event tests for lower and upper cases

--- src/kata_coffee/coffee.py
+++ src/kata_coffee/coffee.py
@@ -1,3 +1,8 @@
 def how_much_coffee(events):
+    if "cw" or "cat" or "dog" or "movie":
+        return 1
+    elif "CW" or "CAT" or "DOG" or "MOVIE":
+        return 2
+    else:
+        return 0
 
-    return 1
--- tests/test_coffee.py
+++ tests/test_coffee.py
@@ -3,3 +3,18 @@ from src.kata_coffee.coffee import how_much_coffee
 
 def test_solve_some_kata_lower_case():
     assert how_much_coffee(["cw"]) == 1
+
+def test_walk_a_cat_lower_case():
+    assert how_much_coffee(["cat"]) == 1
+
+def test_walk_a_dog_lower_case():
+    assert how_much_coffee(["dog"]) == 1
+
+def test_watch_a_movie_lower_case():
+    assert how_much_coffee(["movie"]) == 1
+
+def test_solve_some_kata_upper_case():
+    assert how_much_coffee(["CW"]) == 1
+
+def test_walk_a_cat_upper_case():
+    assert how_much_coffee(["CAT"]) == 1
